5df9dee8178d4d541c10988e67dd4276babb3b65
dhananjay12 <dhananjay1204@gmail.com>
2019-06-30 14:49:40 +0200

    Eureka only

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 kubectl apply -f k8s
-kubectl set image deployments/contactus contactus=dhananjay12/contact-us-service:$SHA
-kubectl set image deployments/user user=dhananjay12/user-service:$SHA
-kubectl set image deployments/zuul zuul=dhananjay12/zuul-server:$SHA
+kubectl set image deployments/contactus contactus=dhananjay12/fungible/contact-us-service:$SHA
+kubectl set image deployments/user user=dhananjay12/fungible/user-service:$SHA
+kubectl set image deployments/zuul zuul=dhananjay12/fungible/zuul-server:$SHA
